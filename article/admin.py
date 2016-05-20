b10aa5575157bb58bf5630671638cf4233536e1d
xuefeng.yang <xuefeng.yang@xundayuncheng.com>
2016-05-20 12:48:22 +0800

    完善

--- article/admin.py
+++ article/admin.py
@@ -2,24 +2,24 @@ from django.contrib import admin
 
 from article.models import Author,Tag,Classification,Article
 # Register your models here.
-#from django_summernote.admin import SummernoteModelAdmin
+# from django_summernote.admin import SummernoteModelAdmin
 admin.site.register(Author)
 admin.site.register(Tag)
 admin.site.register(Classification)
 
-#class ArticleAdmin(SummernoteModelAdmin):
-      #pass
-#admin.site.register(Article,ArticleAdmin)
+
+# class ArticleAdmin(SummernoteModelAdmin):
+#     pass
+# admin.site.register(Article,ArticleAdmin)
+
 
 class ArticleAdmin(admin.ModelAdmin):
-      class Media:
-         js = (
-
-         '/static/tinymce/tinymce.min.js',
-         '/static/tinymce/config.js',
-        
-         )
-admin.site.register(Article,ArticleAdmin)
+    class Media:
+        js = (
+            '/static/tinymce/tinymce.min.js',
+            '/static/tinymce/config.js',
+        )
+admin.site.register(Article, ArticleAdmin)
      
 
 
