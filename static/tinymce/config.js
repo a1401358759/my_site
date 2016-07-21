0fd763ab9cfdc8519e4281f11999de8adf907f4c
xuefeng.yang <xuefeng.yang@xundayuncheng.com>
2016-07-21 11:26:02 +0800

    后台添加syntaxhighlighter

--- static/tinymce/config.js
+++ static/tinymce/config.js
@@ -5,12 +5,12 @@ tinyMCE.init({
     plugins: [
         "advlist autolink autosave link image lists charmap print preview hr anchor pagebreak spellchecker",
         "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
-        "table contextmenu directionality emoticons template textcolor paste fullpage textcolor save"
+        "table contextmenu directionality emoticons template textcolor paste fullpage save codesample"
     ],
     //language: 'zh_CN',
-    menubar: false,
+    //menubar: false,
     toolbar_items_size: 'small',
     toolbar1: " save undo redo | cut copy paste | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect formatselect fontselect fontsizeselect",
-    toolbar2: " searchreplace | bullist numlist | outdent indent blockquote | link unlink anchor image media code | inserttime preview | forecolor backcolor",
-    oolbar3: "table | hr removeformat | subscript superscript | charmap emoticons | print fullscreen | ltr rtl | spellchecker | visualchars visualblocks nonbreaking template pagebreak restoredraft"
+    toolbar2: " searchreplace | bullist numlist | outdent indent blockquote | link unlink anchor image media code | inserttime preview | forecolor backcolor | codesample",
+    toolbar3: "table | hr removeformat | subscript superscript | charmap emoticons | print fullscreen | ltr rtl | spellchecker | visualchars visualblocks nonbreaking template pagebreak restoredraft"
 });
