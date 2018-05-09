46828f3bbfc35d0b6fb4023dbe5cc97a38466f38
xuefeng.yang <xuefeng.yang@zhengyi.tech>
2018-05-09 15:08:29 +0800

    更换为mysql数据库

--- my_site/settings.py
+++ my_site/settings.py
@@ -78,10 +78,30 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+MYSQLDB_CONNECT_TIMEOUT = 1
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'CONN_MAX_AGE': 3600,
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my-site',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'TEST': {
+            'CHARSET': 'utf8',
+            'COLLATION': 'utf8_general_ci'
+        },
+        'OPTIONS': {
+            'connect_timeout': MYSQLDB_CONNECT_TIMEOUT,
+        }
     }
 }
 
