f11caae3c0ff61847050f0ba10a4225597db1a30
xuefeng.yang <xuefeng.yang@zhengyi.tech>
2018-05-12 10:19:14 +0800

    fix bug

--- my_site/local_settings.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# SESSION_CACHE_ALIAS = 'default'
-# CACHE_MIDDLEWARE_SECONDS = 0  # 单位秒
-# CACHE_MIDDLEWARE_KEY_PREFIX = 'my_site'
---
+++ var/www/example.com/static/tinymce/config.js
@@ -0,0 +1,16 @@
+tinyMCE.init({
+    selector: 'textarea',
+    theme : "modern",
+    height: 300,
+    plugins: [
+        "advlist autolink autosave link image lists charmap print preview hr anchor pagebreak spellchecker",
+        "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+        "table contextmenu directionality emoticons template textcolor paste fullpage save codesample"
+    ],
+    //language: 'zh_CN',
+    //menubar: false,
+    toolbar_items_size: 'small',
+    toolbar1: " save undo redo | cut copy paste | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect formatselect fontselect fontsizeselect",
+    toolbar2: " searchreplace | bullist numlist | outdent indent blockquote | link unlink anchor image media code | inserttime preview | forecolor backcolor | codesample",
+    toolbar3: "table | hr removeformat | subscript superscript | charmap emoticons | print fullscreen | ltr rtl | spellchecker | visualchars visualblocks nonbreaking template pagebreak restoredraft"
+});
