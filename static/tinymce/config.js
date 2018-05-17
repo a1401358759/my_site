4965282c3ad87df976f9a00b8e0b78623c904671
xuefeng.yang <xuefeng.yang@zhengyi.tech>
2018-05-17 21:06:45 +0800

    代码不换行，修改editor高度

--- static/tinymce/config.js
+++ static/tinymce/config.js
@@ -1,11 +1,11 @@
 tinyMCE.init({
     selector: 'textarea',
     theme : "modern",
-    height: 300,
+    height: 450,
     plugins: [
-        "advlist autolink autosave link image lists charmap print preview hr anchor pagebreak spellchecker",
-        "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
-        "table contextmenu directionality emoticons template textcolor paste fullpage save codesample"
+      "advlist autolink autosave link image lists charmap print preview hr anchor pagebreak spellchecker",
+      "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+      "table contextmenu directionality emoticons template textcolor paste fullpage save codesample"
     ],
     toolbar_items_size: 'small',
     toolbar1: " save undo redo | cut copy paste | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect formatselect fontselect fontsizeselect",
