f12c89559adb09e93a7c5009d6195cbec1ea9e4b
xuefeng.yang <xuefeng.yang@zhengyi.tech>
2018-05-23 19:52:16 +0800

    优化代码

--- utils/mine_qiniu.py
+++ utils/mine_qiniu.py
@@ -7,10 +7,6 @@ from qiniu import Auth, put_data
 # 需要填写你的 Access Key 和 Secret Key
 access_key = 'D5m1nrbqTRDIQ1OdpeOM5eN8BL9X3KeLI6b7bwAF'
 secret_key = 'VPLtQM3lwp9arR8qMeVSFKelQWO2tXpeC_yLAdo5'
-
-# 构建鉴权对象
-q = Auth(access_key, secret_key)
-# 要上传的空间
 bucket_name = 'blog'
 domain_prefix = 'https://p966opuom.bkt.clouddn.com/'
 
