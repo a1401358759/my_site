7a06ce71fbaab43699d0f3ccf582f6d877ce3978
xuefeng.yang <xuefeng.yang@zhengyi.tech>
2018-08-06 20:49:11 +0800

    修改静态文件位置

--- my_site/settings.py
+++ my_site/settings.py
@@ -128,7 +128,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "var/www/static/"
+STATIC_ROOT = "var/static/"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
