acc9d7f0003e834c8e7071a17e1d57011d7b6e36
xuefeng.yang <xuefeng.yang@zhengyi.tech>
2018-12-04 21:12:23 +0800

    修改七牛云存储文件名

--- utils/mine_qiniu.py
+++ utils/mine_qiniu.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 
-import time
+import random
+# import time
 from qiniu import Auth, put_data
 
 
 # 需要填写你的 Access Key 和 Secret Key
 access_key = 'D5m1nrbqTRDIQ1OdpeOM5eN8BL9X3KeLI6b7bwAF'
 secret_key = 'VPLtQM3lwp9arR8qMeVSFKelQWO2tXpeC_yLAdo5'
-bucket_name = 'blog'
+# bucket_name = 'blog'
 domain_prefix = 'http://img.yangsihan.com/'
 
 
 def upload_data(data, bucket_name):
     # 生成上传凭证
     q = Auth(access_key, secret_key)
-    key = str(int(time.time()))
+    key = ''.join(random.sample('0123456789abcdefghijklmnopqrstuvwxyz', 8)) + '.png'
     token = q.upload_token(bucket_name, key)
     # 上传文件
     retData, respInfo = put_data(token, key, data)
