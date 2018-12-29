8d5dae9b6d47ba1a60080eee2f1565b5b0955d02
xuefeng.yang <xuefeng.yang@zhengyi.tech>
2018-12-29 11:29:25 +0800

    修改样式

--- article/backends.py
+++ article/backends.py
@@ -7,7 +7,7 @@ from models import Tag, Music
 
 def get_tags_and_musics():
     tag_list = list(Tag.objects.all())
-    random.shuffle(tag_list)
+    random.shuffle(tag_list)  # random.shuffle()的返回值是none，改变的是原来的元素
 
     color_array, music_list = [
         '0', '1', '2', '3', '4', '5', '6', '7',
@@ -15,7 +15,7 @@ def get_tags_and_musics():
     ], []
 
     for tag in tag_list:
-        tag.color = '#' + ''.join(random.sample(color_array, 6))
+        tag.color = '#' + ''.join(random.sample(color_array, 6))  # 为每个标签随机生成颜色
 
     musics = Music.objects.all()
     for item in musics:
@@ -27,4 +27,5 @@ def get_tags_and_musics():
             "lrc": item.lrc,
         })
 
+    random.shuffle(music_list)
     return tag_list, json.dumps(music_list)
