95fe0f622dc7fabd84877639336afb7c9332f3ed
xuefeng.yang <xuefeng.yang@zhengyi.tech>
2019-01-11 14:28:13 +0800

    提出公共js

---
+++ var/static/js/valine.js
@@ -0,0 +1,10 @@
+new Valine({
+  el:'#vcomments',
+  appId:'2pSOFwxMUB0mHbMHpCH9bhRL-gzGzoHsz',
+  appKey:'zkBqvFzJ7NswaFW6oGICyKpq',
+  notify:true,
+  avatar:'robohash',
+  placeholder:'来说两句吧······',
+  visitor:true,
+  lang:'zh-cn'
+});
