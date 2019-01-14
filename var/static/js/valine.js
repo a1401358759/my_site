113daa3ff66a53a5d6e5db4a30b10aacdc5ffcdf
xuefeng.yang <xuefeng.yang@zhengyi.tech>
2019-01-14 12:20:11 +0800

    修改valine默认样式

--- var/static/js/valine.js
+++ var/static/js/valine.js
@@ -2,9 +2,10 @@ new Valine({
   el:'#vcomments',
   appId:'2pSOFwxMUB0mHbMHpCH9bhRL-gzGzoHsz',
   appKey:'zkBqvFzJ7NswaFW6oGICyKpq',
-  notify:true,
-  avatar:'robohash',
-  placeholder:'来说两句吧······',
+  notify:false,
+  verify: false,
+  avatar:'monsterid',
+  placeholder:'ヾﾉ≧∀≦)o来啊，快活啊!',
   visitor:true,
   lang:'zh-cn'
 });
