6cd0b2e496c8b6940af694afba7679aaa8026932
xuefeng.yang <xuefeng.yang@zhengyi.tech>
2019-03-18 17:21:21 +0800

    markdown与发稿量

--- article/templatetags/custom_markdown.py
+++ article/templatetags/custom_markdown.py
@@ -1,9 +1,9 @@
 # coding:utf-8
-import markdown
 
+import markdown
+import mistune
 from django import template
 from django.template.defaultfilters import stringfilter
-from django.utils.encoding import force_text
 from django.utils.safestring import mark_safe
 
 register = template.Library()  # 自定义filter时必须加上
@@ -16,8 +16,15 @@ def custom_markdown(value):
         value, extensions=[
             'markdown.extensions.extra',
             'markdown.extensions.codehilite',
-            'markdown.extensions.toc'
+            'markdown.extensions.toc',
+            'markdown.extensions.tables'
         ],
         safe_mode=True,
         enable_attributes=False)
     )
+
+
+@register.filter(is_safe=True)  # 注册template filter
+def markdown_to_html(value):
+    markdown = mistune.Markdown()
+    return mark_safe(markdown(value))
