467351768ecebcfdd48a9f34ea5aca0e12bf4c18
xuefeng.yang <xuefeng.yang@zhengyi.tech>
2019-04-11 10:40:27 +0800

    修改包版本

--- my_site/settings.py
+++ my_site/settings.py
@@ -101,6 +101,24 @@ CACHES = {
 # celery config
 from config.celery_conf import *
 
+# Password validation
+# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
@@ -142,8 +160,8 @@ ADMINS = {('Daniel', '1401358759@qq.com')}
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['templates'],
         'APP_DIRS': True,
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'OPTIONS': {
             'context_processors': [
                 "django.template.context_processors.debug",
