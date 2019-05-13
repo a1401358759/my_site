1235144242eeafdbe35d1a67904ec946b9541d46
xuefeng.yang <xuefeng.yang@zhengyi.tech>
2019-05-13 15:21:20 +0800

    提出公共的js

---
+++ static/js/add-comments.js
@@ -0,0 +1,41 @@
+// 设置当前页面路径
+$("#target").val(window.location.pathname);
+// jQuery emoji 解析
+jQuery(".needEmojiParse").emojiParse({
+  basePath: '/static/plugins/jQuery-emoji/images/emoji',
+  icons: emojiLists   // 注：详见 js/emoji.list.js
+});
+// jquery emoji 初始化
+jQuery("#comment_content").emoji({
+  showTab: true,
+  animation: 'fade',
+  basePath: '/static/plugins/jQuery-emoji/images/emoji',
+  icons: emojiLists  // 注：详见 js/emoji.list.js
+});
+// 设置评论父级id
+function set_parent_comment_id(comment_id, nickname) {
+  var placeholder = "@" + nickname;
+  document.getElementById('parent_comment_id').value = comment_id;
+  jQuery("#comment_content").attr("placeholder", placeholder).focus();
+  window.location.href = "#content-textarea";
+};
+// 提交评论
+function add_comments(){
+  var formData = jQuery('#comment_form').serialize();
+  jQuery.ajax({
+    url: "/add-comments/",
+    type: 'POST',
+    data: formData,
+    success: function (data) {
+      if (data.code == 0) {
+        toastr["success"]("评论成功！");
+        setTimeout(function(){window.location.reload();}, 1000);
+      } else {
+        toastr["warning"](data.msg_cn);
+      }
+    },
+    error: function () {
+      toastr["error"]("网络错误！");
+    }
+  });
+}
