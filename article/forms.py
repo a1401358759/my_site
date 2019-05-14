739086afa7ffa0edf9982f4ec3df73cadfc4c883
xuefeng.yang <xuefeng.yang@zhengyi.tech>
2019-05-14 08:45:02 +0800

    评论添加长度限制

--- article/forms.py
+++ article/forms.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from django import forms
+from utils.dlibs.forms.validators import email_validator
 
 
 class CommentForm(forms.Form):
-    nickname = forms.CharField(label=u'昵称')
-    email = forms.CharField(label=u'邮箱')
-    website = forms.CharField(label=u'网站', required=False)
-    content = forms.CharField(label=u'评论内容')
+    nickname = forms.CharField(label=u'昵称', max_length=20)
+    email = forms.CharField(label=u'邮箱', validators=[email_validator])
+    website = forms.CharField(label=u'网站', required=False, max_length=50)
+    content = forms.CharField(label=u'评论内容', max_length=150)
     target = forms.CharField()
     parent_comment_id = forms.IntegerField(required=False)
