9cfa6e5f7cfba668ef9b6f1dbffa6315d2104007
xuefeng.yang <xuefeng.yang@zhengyi.tech>
2019-05-15 14:52:02 +0800

    修改图片请求协议

--- static/js/valine.js
+++ static/js/valine.js
@@ -8,7 +8,7 @@ new Valine({
   avatar:'mp',
   placeholder: 'ヾﾉ≧∀≦)o来啊，快活啊!',
   visitor: true,
-  emoticon_url: "http://emoji.yangsihan.com/",
+  emoticon_url: "https://emoji.yangsihan.com/",
   emoticon_list: [
     '1.png','2.png','3.png','4.png','5.png','6.png','7.png','8.png','9.png','10.png','11.png','12.png','13.png','14.png',
     '15.png','16.png','17.png','18.png','19.png','20.png','21.png','22.png','23.png','24.png','25.png','26.png','27.png','28.png','29.png',
