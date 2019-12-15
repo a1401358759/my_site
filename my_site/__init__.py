1dadabd70191067b9be072b96c03fba5930f1412
xuefeng.yang <xuefeng.yang@zhengyi.tech>
2019-12-15 21:17:29 +0800

    添加配置

--- my_site/__init__.py
+++ my_site/__init__.py
@@ -1,4 +1,7 @@
 from __future__ import absolute_import
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
