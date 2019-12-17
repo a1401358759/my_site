49b1bc90d010e247f101209e63591aff7a88c41e
yangxf <yangxf@cmgos.com>
2019-12-17 15:15:34 +0800

    修改包版本

--- my_site/__init__.py
+++ my_site/__init__.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import
-import pymysql
 
-pymysql.install_as_MySQLdb()
+# 使用PyMySQL连接数据库时需要打开下面两条注释， 目前使用mysqlclient连接数据库
+# import pymysql
+# pymysql.install_as_MySQLdb(MySQLdb_version=(1, 4, 2, "final", 0))
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
